91681fb6b6111645029f4b130ab99245e3dd35ad
Szymon Dąbrowski <s31562@pjwstk.edu.pl>
2025-03-19 14:42:15 +0100

    Initial commit

---
+++ cwiczenia3/cwiczenia3/Container.cs
@@ -0,0 +1,14 @@
+﻿namespace cwiczenia3;
+
+public class Container
+{
+    private double loadWeight;
+    private double height;
+    private double contWeight;
+    private double depth;
+    private double maxLoadWeight;
+    
+    
+    
+    
+}
